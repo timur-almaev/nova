881a9c44ee2144122f4c9cb9de91059b448db5be
Smith Thomas <Thomas.Smith3@nottingham.ac.uk>
2017-01-23 14:12:50 +0000

    New Teir window updated to aaccomadate new geometric annotation buttons

--- Types/AnnoScheme.cs
+++ Types/AnnoScheme.cs
@@ -14,6 +14,10 @@ namespace ssi
             DISCRETE,
             FREE,
             CONTINUOUS,
+            POINT,
+            POLYGON,
+            GRPAH,
+            SEGMENTATION,
         }
 
         public class Label
