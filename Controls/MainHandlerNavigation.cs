ef3fda4310425c90a40530596f2690dd649eb337
Smith Thomas <Thomas.Smith3@nottingham.ac.uk>
2017-01-23 16:25:21 +0000

    creation of geometric scheme option windows

--- Controls/MainHandlerNavigation.cs
+++ Controls/MainHandlerNavigation.cs
@@ -102,7 +102,8 @@ namespace ssi
                                 addAnnoTier(al);
                             }
                         }
-                        else if (annoType == AnnoScheme.TYPE.CONTINUOUS)
+                        else if (annoType == AnnoScheme.TYPE.CONTINUOUS || annoType == AnnoScheme.TYPE.POINT || annoType == AnnoScheme.TYPE.POLYGON ||
+                                 annoType == AnnoScheme.TYPE.GRPAH || annoType == AnnoScheme.TYPE.SEGMENTATION)
                         {
                             double defaultSr = 25.0;
 
@@ -115,13 +116,66 @@ namespace ssi
                                 }
                             }
 
-                            AnnoTierNewContinuousSchemeWindow.Input input = new AnnoTierNewContinuousSchemeWindow.Input() { SampleRate = defaultSr, MinScore = 0.0, MaxScore = 1.0, MinColor = Colors.LightBlue, MaxColor = Colors.Red };
-                            AnnoTierNewContinuousSchemeWindow dialog2 = new AnnoTierNewContinuousSchemeWindow(input);
-                            dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-                            dialog2.ShowDialog();
-                            if (dialog2.DialogResult == true)
+                            AnnoScheme annoScheme = null;
+
+                            if (annoType == AnnoScheme.TYPE.CONTINUOUS)
                             {
-                                AnnoScheme annoScheme = dialog2.Result;
+                                AnnoTierNewContinuousSchemeWindow.Input input = new AnnoTierNewContinuousSchemeWindow.Input() { SampleRate = defaultSr, MinScore = 0.0, MaxScore = 1.0, MinColor = Colors.LightBlue, MaxColor = Colors.Red };
+                                AnnoTierNewContinuousSchemeWindow dialog2 = new AnnoTierNewContinuousSchemeWindow(input);
+                                dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                                dialog2.ShowDialog();
+                                if (dialog2.DialogResult == true)
+                                {
+                                    annoScheme = dialog2.Result;
+                                }
+                            }
+                            else if (annoType == AnnoScheme.TYPE.POINT)
+                            {
+                                AnnoTierNewPointSchemeWindow.Input input = new AnnoTierNewPointSchemeWindow.Input() { SampleRate = defaultSr, NumPoints = 0.0, Color = Colors.Green};
+                                AnnoTierNewPointSchemeWindow dialog2 = new AnnoTierNewPointSchemeWindow(input);
+                                dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                                dialog2.ShowDialog();
+                                if (dialog2.DialogResult == true)
+                                {
+                                    annoScheme = dialog2.Result;
+                                }
+                            }
+                            else if (annoType == AnnoScheme.TYPE.POLYGON)
+                            {
+                                AnnoTierNewPolygonSchemeWindow.Input input = new AnnoTierNewPolygonSchemeWindow.Input() { SampleRate = defaultSr, NumNodes = 0.0, NodeColour = Colors.Green, LineColour = Colors.Red };
+                                AnnoTierNewPolygonSchemeWindow dialog2 = new AnnoTierNewPolygonSchemeWindow(input);
+                                dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                                dialog2.ShowDialog();
+                                if (dialog2.DialogResult == true)
+                                {
+                                    annoScheme = dialog2.Result;
+                                }
+                            }
+                            else if (annoType == AnnoScheme.TYPE.GRPAH)
+                            {
+                                AnnoTierNewGraphSchemeWindow.Input input = new AnnoTierNewGraphSchemeWindow.Input() { SampleRate = defaultSr, NumNodes = 0.0, NodeColour = Colors.Green, LineColour = Colors.Red };
+                                AnnoTierNewGraphSchemeWindow dialog2 = new AnnoTierNewGraphSchemeWindow(input);
+                                dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                                dialog2.ShowDialog();
+                                if (dialog2.DialogResult == true)
+                                {
+                                    annoScheme = dialog2.Result;
+                                }
+                            }
+                            else if (annoType == AnnoScheme.TYPE.SEGMENTATION)
+                            {
+                                AnnoTierNewSegmentationSchemeWindow.Input input = new AnnoTierNewSegmentationSchemeWindow.Input() { SampleRate = defaultSr, MinScore = 0.0, MaxScore = 1.0, MinColor = Colors.LightBlue, MaxColor = Colors.Red };
+                                AnnoTierNewSegmentationSchemeWindow dialog2 = new AnnoTierNewSegmentationSchemeWindow(input);
+                                dialog2.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                                dialog2.ShowDialog();
+                                if (dialog2.DialogResult == true)
+                                {
+                                    annoScheme = dialog2.Result;
+                                }
+                            }
+
+                            if (annoScheme != null)
+                            {
                                 AnnoList annoList = new AnnoList() { Scheme = annoScheme };
                                 addAnnoTier(annoList);
                             }
